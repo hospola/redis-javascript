b86ec5f7817d093432d5238da07818e1eec60873
marwan <marwan.aliaoui@estudiantat.upc.edu>
2025-06-24 23:13:21 +0200

    second step, multiple ping

--- app/main.js
+++ app/main.js
@@ -6,13 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
    // Handle connection
-   connection.write("+PONG\r\n");
+   connection.on("data", (data) => {
+     connection.write("+PONG\r\n");
+   });
  });
 //
  server.listen(6379, "127.0.0.1");
-
-
- // cuando llegue cualquier comando responder pong
-    server.on("data", (data) => {
-        server.write("+PONG\r\n");
-    });
